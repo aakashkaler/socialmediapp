fd75650335fd6ca97b73b8602470502324bf7444
aakashkaler <68342702+aakashkaler@users.noreply.github.com>
2021-04-08 14:05:46 +0530

    setup static files access for layouts and pages

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const expressLayouts = require('express-ejs-layouts');
+
+//database mongodb
+app.use(express.static('./assets'));
+//layout
+app.use(expressLayouts);
+//extract styles and script from subpages into layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
+// use express router
+app.use('/',require('./routes'));
+
+//set view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err)
 {
