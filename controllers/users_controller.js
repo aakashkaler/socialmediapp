bf31c2fe6d2d8ad0bae12ae318c4a643003c91c4
aakashkaler <68342702+aakashkaler@users.noreply.github.com>
2021-04-26 14:12:11 +0530

    codeial social media

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,9 +4,31 @@ const User = require('../models/users');
 
 module.exports.profile=function(req,res)
 {
-    return res.render('user_profile',{
-        title:"home"
-    });
+    if(req.cookies.user_id)
+    {
+       User.findById(req.cookies.user_id,function(err,user)
+       { 
+           if(err)
+           {
+               console.log('error ');
+               return ;
+           }
+
+        if(user)
+        {
+            return res.render('user_profile',{
+                title:"User profile",
+                user:user
+            });
+        }
+        return res.redirect('/users/sign-in');
+       });
+    }
+    else
+    {
+        return res.redirect('/users/sign-in');
+    }
+    
 }
 
 //render sing up page
@@ -61,5 +83,35 @@ module.exports.create =function(req,res)
 //get the sign up data
 module.exports.createSession =function(req,res)
 {
+    //find the users
+    User.findOne({email: req.body.email},function(err,user)
+    {
+       if(err)
+       {
+           console.log('user not found in sign in');
+           return ;
+       }
+
+       //handel user if found
+       if(user)
+       {
+           // if found handel password matching 
+           if(user.password!=req.body.password)
+           {
+               return redirect('back');
+           }
+
+           //handel sesaon creation
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+       }
+       else
+       {
+           //handel user if not found
+            return res.redirect('back');
+       }
+    });
+    
+
     
 }
